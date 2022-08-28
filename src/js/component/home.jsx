671c68bbe89f8512f6b031cb011ce4d3b1c331aa
Charlytoc <charlyjchaconc@gmail.com>
2022-08-28 20:59:32 +0000

    I need help

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import TrafficLight from "./trafficLight.jsx";
+
 
 //create your first component
 const Home = () => {
+
   const [color, setColor] = useState("nothing")
+  const [turnedOn, setTurnedOn] = useState(true)
 
   const redSelected = () => {
     setColor("red")
@@ -30,51 +30,45 @@ const Home = () => {
   const turningOff = () => {
     setColor(null)
   }
+
   let index = 0
 
- 
-  // const animation = () => {
-  //   setInterval( () => {
-      // let coloursArray = ["red", "yellow", "green"];
-      // setColor(coloursArray[index]);
-      // index++;
-      // if (color == null) {clearInterval(this)}
-      // if (index > 2) {index = 0}}, 1000)
   
-  // }
+  const turnOn = () => {
 
-  let interval;
+    if (turnedOn === false) {
+      setTurnedOn(true)
+    }
+    else {setTurnedOn(false)}
 
-
-  const [startLighting, setStartLighting] = useState("off")
-
-  const changeState = () => {
-    if (startLighting == "off") {
-      interval = setInterval ( () => {
+    if (turnedOn === true) {
+      setInterval ( () => {
         let coloursArray = ["red", "yellow", "green"];
-      setColor(coloursArray[index]);
-      index++;
-      if (index > 2) {index = 0}
-      }, 1000)
-      // startLighting = "on";
-      setStartLighting("on");
-      console.log(startLighting)
-   }
-   else {setStartLighting("off"); 
-   clearInterval(interval)
-  console.log(startLighting)
-  }
-  }
+          setColor(coloursArray[index]);
+          index++;
+          if (index > 2) {index = 0}
+          }, 1000)
+    }
+
+    else {clearInterval( () => {
+      let coloursArray = ["red", "yellow", "green"];
+        setColor(coloursArray[index]);
+        index++;
+        if (index > 2) {index = 0}
+        }, 1000)}
   
-  // Aún no logro detener el setInterval, empiezo desesperarme. 
+  }
+ 
 
-
-  // const stopAnimation = () => {
-  //   console.log(interval)
-  //   clearInterval(interval)
-  //   console.log(interval)
-  // }
-
+  // NOTA PARA EL QUE REVISE, NO ME SIRVE EL CLEAR INTERVAAAAL
+// NOTA PARA EL QUE REVISE, NO ME SIRVE EL CLEAR INTERVAAAAL
+// NOTA PARA EL QUE REVISE, NO ME SIRVE EL CLEAR INTERVAAAAL
+// NOTA PARA EL QUE REVISE, NO ME SIRVE EL CLEAR INTERVAAAAL
+// NOTA PARA EL QUE REVISE, NO ME SIRVE EL CLEAR INTERVAAAAL
+// NOTA PARA EL QUE REVISE, NO ME SIRVE EL CLEAR INTERVAAAAL
+// NOTA PARA EL QUE REVISE, NO ME SIRVE EL CLEAR INTERVAAAAL
+// NOTA PARA EL QUE REVISE, NO ME SIRVE EL CLEAR INTERVAAAAL
+// NOTA PARA EL QUE REVISE, NO ME SIRVE EL CLEAR INTERVAAAAL
 
 	return (<>
     
@@ -82,7 +76,7 @@ const Home = () => {
       
       <div className="botones">
         <span className="buttons" id="turn-off-button" onClick={turningOff}>Turn off</span>
-        <span className="buttons" id="start-button" onClick={changeState}>Start animation</span>
+        <span className="buttons" id="start-button" onClick={turnOn}>Start animation</span>
         {/* <span className="buttons" id="stop-button" onClick={stopAnimation}>Stop animation</span> */}
 
       </div>
@@ -96,14 +90,13 @@ const Home = () => {
       <div className="floor"></div>
        
     </div>
-    <div className="container">
+    {/* <div className="container">
             <div className="container bg-dark" style={{width: 67}}>
-                <div className="rounder-circle bg-light" style={{width: 50, height: 50}} ><div className="rounded-circle bg-success" style={{width: 45, height: 45}} onClick={greenSelected}></div></div>
+                <div className="rounded-circle bg-success" style={{width: 45, height: 45}} onClick={greenSelected}></div>
                 <div><div className="rounded-circle bg-warning" style={{width: 45, height: 45}} onClick={yellowSelected}></div></div>
                 <div><div className="rounded-circle bg-danger" style={{width: 45, height: 45}} onClick={redSelected}></div></div>
-            </div>
-        </div>
-    {/* <TrafficLight funct={redColored}/> */}
+            </div> */}
+        {/* </div> */}
     </>
 	);
 };
